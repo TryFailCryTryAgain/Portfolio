930feb109685581f6ffb0761848b8de9fa80b871
David Segerbo <david.k.segerbo@gmail.com>
2026-09-07 15:54:40 +0200

    updated routes

--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router';
 import App from '../App';
+import { RouterContainer } from './router_container';
+import Work_Window from '../components/Work_Window';
 
 const AppRoutes = () => {
     return (
         <Routes>
 
             <Route index element={<App />}/>
-            {/* <Route path={RouterContainer.work} element={<Work_Window />}/> */}
+            <Route path={RouterContainer.work} element={<Work_Window />}/>
 
         </Routes>
     )
